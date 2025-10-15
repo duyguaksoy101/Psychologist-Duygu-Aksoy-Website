5f0282e48413f2e84cb4d5b7a8467b752e693285
Duygu Aksoy <duyguaksoy101@gmail.com>
2025-10-15 15:48:49 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import './index.css';
 import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import { createRoot } from 'react-dom/client';
+import { App } from '@/App.tsx';
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
