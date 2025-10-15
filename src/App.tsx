6096b0863d90df759efa43c4398dbe0ee5cea19a
Duygu Aksoy <duyguaksoy101@gmail.com>
2025-10-15 15:30:14 +0300

    Update App.tsx
    Added 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import BlogPage from './pages/BlogPage';
 import ContactPage from './pages/ContactPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import NotFoundPage from './pages/NotFoundPage';
+
 export function App() {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter>
       <div className="flex flex-col min-h-screen bg-white">
         <Navbar />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/blog" element={<BlogPage />} />
             <Route path="/contact" element={<ContactPage />} />
+            {/* The old blog page is removed, and a 404 catch-all route is added */}
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
         <Footer />
       </div>
-    </BrowserRouter>;
+    </BrowserRouter>
+  );
 }
