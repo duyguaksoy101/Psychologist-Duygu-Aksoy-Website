dc225ddec1f160050dbd5b1f9ff77587caafd17c
Duygu Aksoy <duyguaksoy101@gmail.com>
2025-10-17 20:57:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-// src/App.tsx DOSYASINDAKİ DEĞİŞİKLİKLER
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import PrivacyPage from './pages/PrivacyPage';
-import TermsPage from './pages/TermsPage';
+// DEĞİŞİKLİK: Yeni dosya adları ve komponent adları ile importları güncelle
+import HakkimdaPage from './pages/HakkimdaPage';
+import IletisimPage from './pages/IletisimPage';
+import GizlilikPolitikasiPage from './pages/GizlilikPolitikasiPage';
+import KullanimSartlariPage from './pages/KullanimSartlariPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import NotFoundPage from './pages/NotFoundPage';
 import ScrollToTop from './components/ScrollToTop';
-import BlogPage from './pages/BlogPage'; // Import BlogPage
-
-// NOTE: The BlogPostPage component is missing. You will need to create it.
-// import BlogPostPage from './pages/BlogPostPage'; 
+import BlogPage from './pages/BlogPage';
 
 export function App() {
   return (
@@ -25,16 +21,12 @@ export function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* DEĞİŞİKLİK BURADA */}
-            <Route path="/hakkimda" element={<AboutPage />} />
-            <Route path="/iletisim" element={<ContactPage />} />
-            <Route path="/gizlilik-politikasi" element={<PrivacyPage />} />
-            <Route path="/kullanim-sartlari" element={<TermsPage />} />
+            {/* DEĞİŞİKLİK: element={} kısmını yeni komponent adları ile güncelle */}
+            <Route path="/hakkimda" element={<HakkimdaPage />} />
+            <Route path="/iletisim" element={<IletisimPage />} />
+            <Route path="/gizlilik-politikasi" element={<GizlilikPolitikasiPage />} />
+            <Route path="/kullanim-sartlari" element={<KullanimSartlariPage />} />
             <Route path="/blog" element={<BlogPage />} />
-            {/* The route below is commented out because the BlogPostPage component is not yet created.
-              Uncomment it after you create the component.
-            */}
-            {/* <Route path="/blog/:postId" element={<BlogPostPage />} /> */}
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
